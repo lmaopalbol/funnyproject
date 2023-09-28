dcdf3022fd2d0496bd7143703abe7955b9380356
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-09-28 15:08:20 -0400

    Update script.js
    fix imporrt

--- script.js
+++ script.js
@@ -2,18 +2,17 @@
 
 //i had to write the imports like this because proxy was rewriting and breaking the links
 var THREE = await import(
-  "http" + "s://unpkg.com/three@v0.156.1/build/three.module.js"
+  "https://unpkg.com/three@v0.156.1/build/three.module.js"
 );
 
 var MOD1 = await import(
-  "http" + "s://gloomyunwelcomedebuggers.yoshimon44.repl.co/lilMod.js"
+  "/lilMod.js"
 );
 var currentMap = await import(
-  "http" + "s://gloomyunwelcomedebuggers.yoshimon44.repl.co/currentMap/currentMap.js"
+  "/currentMap/currentMap.js"
 );
 var keyboardCuts = await import(
-  "http" +
-"s://gloomyunwelcomedebuggers.yoshimon44.repl.co/currentMap/keyboardCuts.js"
+  "/currentMap/keyboardCuts.js"
 );
 
 print = console.log; //you can use print instead of console.log now
