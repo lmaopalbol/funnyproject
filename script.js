fdc6d5629219c0124ac59c310d848d7e7cdc1df5
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-09-29 20:08:35 -0400

    testing 9/29/23

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // [[CONSTANTS]]
 
+console.log("Heyyyy!!!!! Are you working or WHAT?!");
+
 //i had to write the imports like this because proxy was rewriting and breaking the links
 var THREE = await import(
   "https://unpkg.com/three@v0.156.1/build/three.module.js"
