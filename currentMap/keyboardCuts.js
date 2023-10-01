4ac7ce30f9a60d2f35f510397c52007ddcf4a8f6
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-10-01 16:46:44 -0400

    thinking? / trying to make forward vector (10/1/23) (8)

--- currentMap/keyboardCuts.js
+++ currentMap/keyboardCuts.js
@@ -59,7 +59,7 @@ export function main() {
     var doodoo = new THREE.Vector3(
       -Math.sin(lala.y),
       Math.sin(lala.x),
-      -Math.cos(lala.x) * Math.sin(lala.y)
+      -Math.sin(lala.x) * Math.sin(lala.y)
     );
     
     console.log(doodoo);
